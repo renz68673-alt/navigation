a7e4b8d67cd306cb7541ff3f02ff05f8c834d661
renz68673-alt <renz68673@gmail.com>
2026-07-14 19:57:15 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,110 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "车库外",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.8293162792508539,
+        "pitch": -0.04632849208461387,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3648520050023958,
+          "pitch": 0.36568124847778805,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4443965882083054,
+          "pitch": 0.3247282752383409,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">go straight 10m</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "室内",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -2.370644770862171,
+        "pitch": -0.05127721603142277,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6262384032576511,
+          "pitch": 0.5382832026868787,
+          "rotation": 11.780972450961727,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
